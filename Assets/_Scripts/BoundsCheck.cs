8e0dd3ea7ce62c1845b4324d69eb4efb3e1bb515
Edson Lin <43080740+edsonxx@users.noreply.github.com>
2018-11-02 15:18:02 -0400

    Created enemies and destroy when off screen
    Left off at: Spawning Enemies at Random

--- Assets/_Scripts/BoundsCheck.cs
+++ Assets/_Scripts/BoundsCheck.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
+
     void Awake()
     {
         camHeight = Camera.main.orthographicSize; //b
@@ -16,22 +21,41 @@ public class BoundsCheck : MonoBehaviour {
     void LateUpdate()
     { //d
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
+
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
+
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if (keepOnScreen && !isOnScreen)
+        { 
+            transform.position = pos; 
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
         transform.position = pos;
     }
     // Draw the bounds in the Scene pane using OnDrawGizmos()
@@ -41,4 +65,4 @@ public class BoundsCheck : MonoBehaviour {
         Vector3 boundSize = new Vector3 (camWidth * 2, camHeight * 2, 0.1f);
         Gizmos.DrawWireCube(Vector3.zero, boundSize);
     }
-}
+}
