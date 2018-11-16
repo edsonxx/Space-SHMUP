4df1d402004862b761814a661f155ff14ebdf180
Edson Lin <43080740+edsonxx@users.noreply.github.com>
2018-11-16 14:41:49 -0500

    Not absorbig powerup

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
     [Header("Set in Inspector: Enemy")]
     public float speed = 10f; // The speed in m/s
     public float fireRate = 0.3f; // Seconds/shot (Unused)
     public float health = 10;
     public int score = 100; // Points earned for destroying this
+    public float showDamageDuration = 0.1f;
+
+    [Header("Set Dynamically: Enemy")]
+    public Color[] originalColors;
+    public Material[] materials;
+    public bool showingDamage = false;
+    public float damageDoneTime; // Time to stop showing damage
+    public bool notifiedOfDestruction = false;
+
     protected BoundsCheck bndCheck;
+
 
     void Awake()
     { // b
         bndCheck = GetComponent<BoundsCheck>();
+        materials = Utils.GetAllMaterials(gameObject);
+        originalColors = new Color[materials.Length];
+        for (int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
     }
 
     public Vector3 pos
-    {   
-    get
+    {
+        get
         {
             return (this.transform.position);
         }
@@ -30,15 +47,20 @@ public class Enemy : MonoBehaviour {
     {
         Move();
 
+        if (showingDamage && Time.time > damageDoneTime)
+        {
+            UnShowDamage();
+        }
+
         if (bndCheck != null && bndCheck.offDown)
         {
             {// We're off the bottom, so destroy this GameObject
-            Destroy(gameObject);
+                Destroy(gameObject);
             }
         }
     }
     public virtual void Move()
-    { 
+    {
         Vector3 tempPos = pos;
         tempPos.y -= speed * Time.deltaTime;
         pos = tempPos;
@@ -57,6 +79,8 @@ public class Enemy : MonoBehaviour {
                     break;
                 }
                 // Hurt this Enemy
+                ShowDamage();
+
                 // Get the damage amount from the Main WEAP_DICT.
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0)
@@ -70,4 +94,20 @@ public class Enemy : MonoBehaviour {
                 print("Enemy hit by non-ProjectileHero: " + otherGO.name);
                 break;
         }    }
-}
+    void ShowDamage()
+    {
+        foreach (Material m in materials)
+        {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+    void UnShowDamage() {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
+    }
+}
