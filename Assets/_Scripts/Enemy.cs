87e164a527ebb999f965f7c3c0774aa531b7add3
Edson Lin <43080740+edsonxx@users.noreply.github.com>
2018-11-16 17:40:46 -0500

    Space Shump + Done
    Comments: I unchecked StarfieldBG because it kind ofoverlapps with the Hero. Everything else works

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -10,6 +10,7 @@ public class Enemy : MonoBehaviour
     public float health = 10;
     public int score = 100; // Points earned for destroying this
     public float showDamageDuration = 0.1f;
+    public float powerUpDropChance = 1f;
 
     [Header("Set Dynamically: Enemy")]
     public Color[] originalColors;
@@ -85,6 +86,11 @@ public class Enemy : MonoBehaviour
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0)
                 {
+                    if (!notifiedOfDestruction)
+                    {
+                        Main.S.ShipDestroyed(this);
+                    }
+                    notifiedOfDestruction = true;
                     // Destroy this Enemy
                     Destroy(this.gameObject);
                 }
